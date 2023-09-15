1714db5d27c54ca7bf20fbbf5c172b7482a89d81
CrazzzyyFoxx <craazzzyyfoxx@gmail.com>
2023-09-16 01:11:56 +0300

    Refactor API

--- app/services/api/schemas.py
+++ app/services/api/schemas.py
@@ -48,14 +48,14 @@ class OrderInfo(BaseModel):
 
 
 class OrderPrice(BaseModel):
-    price_booster_dollar_fee: float
+    price_booster_dollar: float
     price_booster_rub: float
     price_booster_gold: float | None = None
 
 
 class PreOrderPrice(BaseModel):
     price_booster_rub: float | None = None
-    price_booster_dollar_fee: float | None = None
+    price_booster_dollar: float | None = None
     price_booster_gold: float | None = None
 
 
