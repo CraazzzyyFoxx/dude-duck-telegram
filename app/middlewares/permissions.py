92d3c5fc1c47fa2f1767e0c4559854847e5253ca
CrazzzyyFoxx <craazzzyyfoxx@gmail.com>
2023-09-16 01:08:03 +0300

    Refactor API

--- app/middlewares/permissions.py
+++ app/middlewares/permissions.py
@@ -17,14 +17,7 @@ class PermissionMessageMiddleware(BaseMiddleware):
             event: Message,
             data: Dict[str, Any]
     ) -> Any:
-        try:
-            user = await api_flows.get_me_user_id(event.from_user.id)
-        except errors.AuthorizationExpired():
-            lang = process_language(event.from_user)
-            await event.answer(render_flows.base("expired", lang))
-            return
-
-        data["user"] = user
+        data["user"] = None
         chat_action = get_flag(data, 'chat_action')
         if chat_action is not None:
             is_superuser = "is_superuser" in chat_action
@@ -71,13 +64,7 @@ class PermissionCallbackMiddleware(BaseMiddleware):
             event: CallbackQuery,
             data: Dict[str, Any]
     ) -> Any:
-        try:
-            user = await api_flows.get_me_user_id(event.from_user.id)
-        except errors.AuthorizationExpired():
-            lang = process_language(event.from_user)
-            await event.answer(render_flows.base("expired", lang), show_alert=True)
-            return
-        data["user"] = user
+        data["user"] = None
         chat_action = get_flag(data, 'chat_action')
         if chat_action is not None:
             is_superuser = "is_superuser" in chat_action
